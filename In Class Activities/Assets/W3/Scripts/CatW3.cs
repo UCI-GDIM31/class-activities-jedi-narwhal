1d7f1f6574864567edb283d1bfd9342373383cb6
jedi-narwhal <soniasnlep@yahoo.com>
2025-10-13 19:43:43 -0700

    final

--- In Class Activities/Assets/W3/Scripts/CatW3.cs
+++ In Class Activities/Assets/W3/Scripts/CatW3.cs
@@ -95,8 +95,7 @@ public class CatW3 : MonoBehaviour
             // Below this comment, CALL the method named DecreaseHealth.
             // Notice this method's return type is void- that means we don't
             //      have to store the result anywhere.
-            
-
+            DecreaseHealth(); 
             // STEP 2 ---------------------------------------------------------
 
             // STEP 6 ---------------------------------------------------------
@@ -107,11 +106,14 @@ public class CatW3 : MonoBehaviour
             //
             // Try toggling the Destroy Cat When Dead setting on the Inspector,
             //      and see how the cat is removed ONLY when it's checked!
-            
+            if (_health <= 0 && _destroyCatWhenDead == true)
+            {
+                DestroyedCat();
+            }
+		  }
 
             // STEP 6 ---------------------------------------------------------
-        }
-    }
+       }
 
     // STEP 3 -----------------------------------------------------------------
     // This method decreases the player's health and updates the UI.
@@ -122,20 +124,20 @@ public class CatW3 : MonoBehaviour
     // In this method:
     // 1. Decrease the value of _health by one point.
     // 2. Uncomment and fix the next line so that it change the value of
-    //      _healthText.text to display "health = " and the new value of _health.
+     //     _healthText.text to display "health = " and the new value of _health.
     //    '_healthText' is a TMP_Text Component, and 'text' is the variable that
     //     decides the text that displays in the UI for this Component.
     //      Hint: there's just one operator missing!
-    private void DecreaseHealth()
+    void DecreaseHealth()
     {
         // write Step 3 below this comment!
-
-
+        _health--;
+        _healthText.text = "health = " + _health;
         // STEP 5 -------------------------------------------------------------
         // Once you've finished Step 4, CALL the GetHealthSpeechText method
         //      and store the result in _speechText's text variable.
         // This will look very similar to the above line to change _healthText ;)
-
+        _speechText.text= GetHealthSpeechText();
 
         // STEP 5 -------------------------------------------------------------
     }
@@ -153,28 +155,35 @@ public class CatW3 : MonoBehaviour
     //      return "OH NO!".
     // 2. Otherwise, return "ouch".
 
-    //private ??? GetHealthSpeechText()
-    //{
-        // put the method body here!
+    string GetHealthSpeechText()
+    {
+        if (_health < _maxHealth / 2)
+        {
+            return "Oh no";
+        }
+        else
+        {
+            return "ouch";
+        }
         
-    //}
+    }
     
     // STEP 4 -----------------------------------------------------------------
 
     // ------------------------------------------------------------------------
-    private void ChangeColor(BallW3 ball)
+     void ChangeColor(BallW3 ball)
     {
         // STEP 7 -------------------------------------------------------------
         // Set the value of the _spriteRenderer's color variable to the value
         //      of the ball's ballRenderer's color variable.
         // This means you'll need to use the '.' twice to get to the color :)
-
+        _spriteRenderer.color = ball.ballRenderer.color;
 
         // STEP 7 -------------------------------------------------------------
     }
     
     // ------------------------------------------------------------------------
-    private void DestroyCat()
+	void DestroyedCat()
     {
         Destroy(gameObject);
     }
