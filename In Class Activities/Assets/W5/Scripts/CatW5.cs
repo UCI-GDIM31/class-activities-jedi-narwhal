dfeea33d6aa6d175b67a64742bff5510e1dc10ee
jedi-narwhal <soniasnlep@yahoo.com>
2025-10-27 19:04:17 -0700

    final inclass asignment

--- In Class Activities/Assets/W5/Scripts/CatW5.cs
+++ In Class Activities/Assets/W5/Scripts/CatW5.cs
@@ -44,9 +44,23 @@ public class CatW5 : MonoBehaviour
         // MULTIPLY one of your vectors with a certain value to do this. >:)
 
         Vector3 translation = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            translation= Vector3.forward * _moveSpeed;
+        }
+    else if (Input.GetKey(KeyCode.S))
+    {
+        translation= Vector3.forward * _turnSpeed;
+    }
+
+        if (_flipWSControls)
+        {
+            translation = translation * -1;
+        }
+        transform.Translate(translation * Time.deltaTime);
         
-
-
+        
         // STEP 1 & 2 ---------------------------------------------------------
 
         float rotation = Input.GetAxis("Horizontal") * _turnSpeed * Time.deltaTime;
